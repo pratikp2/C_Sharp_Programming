26ad5584670a94b17a884c72cc7046f4778f19df
Pratik Patil <pratik.patil@hp.com>
2020-03-02 18:29:07 +0530

    Update

--- C++ CLI/IntroToCLR/2.HandlesAndPointers/HandlesAndPointers.cpp
+++ C++ CLI/IntroToCLR/2.HandlesAndPointers/HandlesAndPointers.cpp
@@ -3,6 +3,7 @@
 	using namespace System;
 
 //ref class means it's a .Net class.
+//Class resides in managed or .Net memory
 public ref class SplitName
 {
 private:
@@ -44,4 +45,8 @@ int main()
 //leaks if they are not properly deleted, since the GC will take care of that, and they don't have
 //a fixed memory address and thus will be moved around during execution.
 
-//To create a new reference of a certain class or value type, we have to allocate it with the "gcnew" keyword
+//To create a new reference of a certain class or value type, we have to allocate it with the "gcnew" keyword.
+// It careates the object of the class in .Net memory and no need to free it it will automatically freed at the
+// time of garbage collection.
+
+//http://www.functionx.com/cppcli/index.htm for detailed study of C++ CLR.
