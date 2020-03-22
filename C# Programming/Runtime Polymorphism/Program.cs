b1a49cd6d02c60f0c29886425b10d1a31134780f
Pratik Patil <pratik.patil@hp.com>
2020-03-22 23:37:11 +0530

    Added Runtime Polymorphism

---
+++ C# Programming/Runtime Polymorphism/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+//Runtime or Dynamic Polymorphism
+
+namespace Runtime_Polymorphism
+{
+    public interface Parent1
+    {
+        public void Method1();
+        public void Method2();
+    }
+
+    public abstract class Parent2
+    {
+        public abstract void Method1();
+        public virtual void Method2() { Console.WriteLine("Called Parent2 Method2"); }
+    }
+
+    public class Parent3
+    {
+        public void Method1() { Console.WriteLine("Called Parent3 Method1"); }
+        public virtual void Method3() { Console.WriteLine("Called Parent3 Method3"); }
+    }
+
+    class Child : Parent3, Parent1
+    {
+        public void Method1() { Console.WriteLine("Called Child Method1"); }
+        public void Method2() { Console.WriteLine("Called Child Method2"); }
+        public override void Method3() { Console.WriteLine("Called Child Method3"); }
+    }
+    class TestCode
+    {
+        static void Main()
+        {
+            Parent1 obj1 = new Child();
+            obj1.Method1();
+            obj1.Method2();
+
+            Parent3 obj2 = new Child();
+            obj2.Method1();
+            obj2.Method3();
+        }
+    }
+}
---
+++ C# Programming/String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname, lname;
+            fname = "Rowan";
+            lname = "Atkinson";
+
+            char[] letters = { 'H', 'e', 'l', 'l', 'o' };
+            string[] sarray = { "Hello", "From", "Tutorials", "Point" };
+
+            string fullname = fname + lname;
+            Console.WriteLine("Full Name: {0}", fullname);
+
+            string greetings = new string(letters);
+            Console.WriteLine("Greetings: {0}", greetings);
+
+            string message = System.String.Join(" ", sarray);
+            Console.WriteLine("Message: {0}", message);
+
+            DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
+            string chat = System.String.Format("Message sent at {0:t} on {0:D}", waiting);
+            Console.WriteLine("Message: {0}", chat);
+        }
+    }
+}
